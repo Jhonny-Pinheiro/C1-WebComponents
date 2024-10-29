cf470edab504828225d86e6b85ed511bc784e888
Jhonny Pinheiro+ <jhonny.saletty@gmail.com>
2024-10-29 11:26:52 +0100

    functions

--- Typescript/build/index.js
+++ Typescript/build/index.js
@@ -1,8 +1,39 @@
 "use strict";
-function printaObjeto(pessoa) {
-    console.log(pessoa);
+let ligado = false;
+let nome = "jhonny";
+let idade = 32;
+let altura = 1.80;
+let nulo = null;
+let indefinido = undefined;
+let retorno;
+let retornoView = 1;
+let produto = {
+    name: "jhonny",
+    cidade: "sp",
+    idade: 30,
+};
+let meuProduto = {
+    nome: "Tenis",
+    preco: 30,
+    unidade: 20,
+};
+let dados = ["felipe", "ana", "jhonny"];
+let dados2 = ["felipe", "ana", "jhonny"];
+let infos = ["felipe", 30, "jhonny", false];
+let boleto = ["agua conta", 20, 30];
+dados.pop();
+let aniversario = new Date("2022-12-01 05:00");
+console.log(aniversario.toString());
+function addNUmber(x, y) {
+    return x + y;
 }
-printaObjeto({
-    name: "Bruce Wayne",
-    vulgo: "Batman"
-});
+let soma = addNUmber(4, 7);
+console.log(soma);
+function addNome(nome) {
+    return `Olá ${nome}`;
+}
+console.log(addNome("Jhonny"));
+function CallToPhone(phone) {
+    return phone;
+}
+console.log(CallToPhone(1131188));
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,14 +1,63 @@
-type heroi = {
-    name: string;
-    vulgo: string;
+let ligado: boolean = false;
+let nome: string = "jhonny";
+let idade: number = 32;
+let altura: number = 1.80;
+
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+let retorno: void 
+let retornoView:any = 1
+
+let produto:object = {
+    name:"jhonny",
+    cidade:"sp",
+    idade: 30,
 };
 
-function printaObjeto(pessoa: heroi) {
-    console.log(pessoa);
+type ProdutoLoja = {
+    nome:string;
+    preco:number;
+    unidade:number;
+};
+
+let meuProduto: ProdutoLoja = {
+    nome: "Tenis",
+    preco: 30,
+    unidade: 20,
+};
+
+let dados:string[] = ["felipe", "ana", "jhonny"];
+let dados2: Array<string>=["felipe", "ana", "jhonny"];
+
+let infos: (string | number | boolean)[]=["felipe",30,"jhonny", false];
+
+let boleto:[string,number,number]=["agua conta", 20, 30];
+
+dados.pop();
+
+let aniversario: Date = new Date("2022-12-01 05:00");
+console.log(aniversario.toString());
+
+function addNUmber(x: number,y:number): number{
+    return x + y;
 }
 
-//teste remove coments
-printaObjeto({ 
-    name: "Bruce Wayne",
-    vulgo: "Batman"
-});
+let soma:number = addNUmber(4,7);
+console.log(soma);
+
+function addNome(nome:string):string{
+    return `Olá ${nome}`;
+}
+
+console.log(addNome("Jhonny"));
+
+function CallToPhone(phone: number | string){
+    return phone;
+}
+
+console.log(CallToPhone(1131188));
+
+async function getDatabase(id:number): Promise <string>{
+    return"jhonny";
+}
